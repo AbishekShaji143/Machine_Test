23d2014e7660c5d987cc16ec1771b8a286a13a8a
Akshay Kumar <akshaykumar.contact@gmail.com>
2025-01-25 00:41:34 +0530

    final finishing

--- asset/js/script.js
+++ asset/js/script.js
@@ -18,8 +18,28 @@ $(document).ready(function(){
             }
         }
     });
-    $('.event-list.owl-carousel').owlCarousel({
+    $('.banner-carousel.owl-carousel').owlCarousel({
         loop:true,
+        margin:0,
+        autoplay:true,
+        nav:false,
+        dots:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            991:{
+                items:1,
+            },
+            1200:{
+                items:1,
+            }
+        }
+    });
+    $('.event-list.owl-carousel').owlCarousel({
+        loop:false,
         margin:10,
         nav:false,
         dots:false,
@@ -43,16 +63,19 @@ $(document).ready(function(){
         }
     });
     $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+        var scroll = $(window).scrollTop();
 
-     //>=, not <=
-    if (scroll >= 200) {
-        //clearHeader, not clearheader - caps H
-        $("header").addClass("fixed-header");
-    }
-    else{
-        $("header").removeClass("fixed-header");
-    }
-}); //missing );
+        //>=, not <=
+        if (scroll >= 200) {
+            //clearHeader, not clearheader - caps H
+            $("header").addClass("fixed-header");
+        }
+        else{
+            $("header").removeClass("fixed-header");
+        }
+    }); //missing );
+    $('.promotion-list-items').on('mouseover', function(){
+        $(this).children('p, a').slideToggle();
+    });
     // add class on click 
 });
